1384ea2dbff8aecb944da713c32faec44e476924
Ali Salahuddin <alisalahuddin20@gmail.com>
2017-03-01 17:18:30 -0800

    main panel code

--- app.R
+++ app.R
@@ -11,6 +11,16 @@ mortality <- read.csv("data/tb-country-mortality.csv", stringsAsFactors = F)
 drug.resistant <- read.csv("data/tb-country-drug-resistant.csv", stringsAsFactors = F)
 
 ui <- fluidPage(
+  titlePanel(""),
+  
+  mainPanel(
+    tabsetPanel(type = "tabs",tabPanel("Introduction"), 
+                tabPanel("Map",fluidRow(column(width = 10, (map) )),
+                               fluidRow(column(width = 10, plotOutput("plot1")))),
+                tabPanel("Plot")
+    
+    )
+  )
   
 )
 
