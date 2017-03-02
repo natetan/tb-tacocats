732ebc5f143a7b11240d95b3820cab43bee73cb6
Ali Salahuddin <alisalahuddin20@gmail.com>
2017-03-01 17:05:18 -0800

    Starter code and files

--- app.R
+++ app.R
@@ -5,3 +5,17 @@ library(dplyr)
 library(shiny)
 library(rsconnect)
 library(ggplot2)
+
+incidence <- read.csv("data/tb-country-incidence.csv", stringsAsFactors = F)
+mortality <- read.csv("data/tb-country-mortality.csv", stringsAsFactors = F)
+drug.resistant <- read.csv("data/tb-country-drug-resistant.csv", stringsAsFactors = F)
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output) {
+  
+}
+
+shinyApp(ui = ui, server = server)
