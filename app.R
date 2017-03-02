112f569fe4791d5abb91f923143bb04ea8e32554
Yulong Tan <yulongtan67@gmail.com>
2017-03-01 17:18:53 -0800

    merge tables

--- app.R
+++ app.R
@@ -5,3 +5,14 @@ library(dplyr)
 library(shiny)
 library(rsconnect)
 library(ggplot2)
+
+tb.incidence <- read.csv('data/tb-country-incidence.csv', stringsAsFactors = FALSE)
+tb.mortality <- read.csv('data/tb-country-mortality.csv', stringsAsFactors = FALSE)
+tb.resistance <- read.csv('data/tb-country-drug-resistant.csv', stringsAsFactors = FALSE)
+
+combined.data <- left_join(tb.incidence, tb.mortality) %>% 
+  left_join(tb.resistance)
+View(combined.data)
+
+countries <- map_data('world') %>% 
+  View()
