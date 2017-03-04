3bf28382e129098f9951395f1b7bc21d31e3e950
Yulong Tan <yulongtan67@gmail.com>
2017-03-04 13:23:07 -0800

    add main template

--- app.R
+++ app.R
@@ -20,16 +20,178 @@ countries <- map_data('world') %>%
 ui <- fluidPage(
   titlePanel(""),
   
-  mainPanel(
-    tabsetPanel(type = "tabs",tabPanel("Introduction"), 
-                tabPanel("Map",fluidRow(column(width = 10, (map) )),
-                               fluidRow(column(width = 10, plotOutput("plot1")))),
-                tabPanel("Plot")
+  tabsetPanel(type = 'tabs',
+    # TAB 1
+    tabPanel(
+      'Panel 1',
+      sidebarLayout(
+        # WIDGET STUFF GOES HERE (inside sidebarPanel)
+        sidebarPanel(
+          selectInput(
+            'testing',
+            label = 'Testing',
+            choices = c('Yes', 'No')
+          )
+        ),
+        # WIDGET STUFF END
+        
+        # VISUAL STUFF HERE (inside mainPanel)
+        mainPanel(
+          selectInput(
+            'testing4',
+            label = 'Testing first panel',
+            choices = c('Yes', 'No')
+          ),
+          tableOutput('table1')
+        )
+        # VISUAL STUFF END
+      )
+    ),
     
+    # TAB 2
+    tabPanel(
+      'Panel 2',
+      sidebarLayout(
+        # WIDGET STUFF GOES HERE (inside sidebarPanel)
+        sidebarPanel(
+          selectInput(
+            'testing2',
+            label = 'Testing 2',
+            choices = c('Yes', 'No')
+          ),
+          selectInput(
+            'testing5',
+            label = 'Second Slider!',
+            choices = c('Yes', 'No')
+          )
+        ),
+        # WIDGET STUFF END
+        
+        # VISUAL STUFF HERE (inside mainPanel)
+        mainPanel(
+          selectInput(
+            'testing3',
+            label = 'Testing second panel',
+            choices = c('Yes', 'No')
+          ),
+          tableOutput('table2')
+        )
+        # VISUAL STUFF END
+      )
+    ),
+    
+    # TAB 3
+    tabPanel(
+      'Panel 3',
+      sidebarLayout(
+        # WIDGET STUFF GOES HERE (inside sidebarPanel)
+        sidebarPanel(
+          selectInput(
+            'testing2',
+            label = 'Testing 2',
+            choices = c('Yes', 'No')
+          ),
+          selectInput(
+            'testing5',
+            label = 'Second Slider!',
+            choices = c('Yes', 'No')
+          )
+        ),
+        # WIDGET STUFF END
+        
+        # VISUAL STUFF HERE (inside mainPanel)
+        mainPanel(
+          selectInput(
+            'testing3',
+            label = 'Testing second panel',
+            choices = c('Yes', 'No')
+          ),
+          tableOutput('table2')
+        )
+        # VISUAL STUFF END
+      )
+    ),
+    
+    # TAB 4
+    tabPanel(
+      'Panel 4',
+      sidebarLayout(
+        # WIDGET STUFF GOES HERE (inside sidebarPanel)
+        sidebarPanel(
+          selectInput(
+            'testing2',
+            label = 'Testing 2',
+            choices = c('Yes', 'No')
+          ),
+          selectInput(
+            'testing5',
+            label = 'Second Slider!',
+            choices = c('Yes', 'No')
+          )
+        ),
+        # WIDGET STUFF END
+        
+        # VISUAL STUFF HERE (inside mainPanel)
+        mainPanel(
+          selectInput(
+            'testing3',
+            label = 'Testing second panel',
+            choices = c('Yes', 'No')
+          ),
+          tableOutput('table2')
+        )
+        # VISUAL STUFF END
+      )
+    ),
+    
+    # TAB 5
+    tabPanel(
+      'Panel 5',
+      sidebarLayout(
+        # WIDGET STUFF GOES HERE (inside sidebarPanel)
+        sidebarPanel(
+          selectInput(
+            'testing2',
+            label = 'Testing 2',
+            choices = c('Yes', 'No')
+          ),
+          selectInput(
+            'testing5',
+            label = 'Second Slider!',
+            choices = c('Yes', 'No')
+          )
+        ),
+        # WIDGET STUFF END
+        
+        # VISUAL STUFF HERE (inside mainPanel)
+        mainPanel(
+          selectInput(
+            'testing3',
+            label = 'Testing second panel',
+            choices = c('Yes', 'No')
+          ),
+          tableOutput('table2')
+        )
+        # VISUAL STUFF END
+      )
     )
   )
-  
 )
+
+# Ali's code
+#ui <- fluidPage(
+#titlePanel(""),
+
+#mainPanel(
+#  tabsetPanel(type = "tabs",tabPanel("Introduction"), 
+#              tabPanel("Map",fluidRow(column(width = 10, (map) )),
+#                       fluidRow(column(width = 10, plotOutput("plot1")))),
+#              tabPanel("Plot")
+              
+#  )
+#)
+
+#)
 
 server <- function(input, output) {
   
